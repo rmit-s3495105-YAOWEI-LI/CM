bce4435b596952d5358ffa829fd90c3904580c3e
Yaowei Li <s3495105@student.rmit.edu.au>
2017-04-03 00:23:44 +1000

    test commit

--- CM/mysite/models.py
+++ CM/mysite/models.py
@@ -6,7 +6,7 @@ from unittest.util import _MAX_LENGTH
 from django.template.defaultfilters import default
 
 # Create your models here.
-class MyUser(models.Model):
+class MyUsesr(models.Model):
     username = models.CharField(max_length=15)
     password=models.CharField(max_length=20)
     permission = models.IntegerField(default=1)
